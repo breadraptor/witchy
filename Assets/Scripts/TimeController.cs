d678692b88fc2cc65e291e827687600c79a31988
Nancy McCollough <nm389@nau.edu>
2015-07-24 13:51:39 -0700

    base commit
    i can move between scenes and count the time!

---
+++ Assets/Scripts/TimeController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using System.Collections;
+
+public class TimeController : MonoBehaviour {
+
+	private System.DateTime startDate;
+	public Text timeDisplay;
+
+	void Awake () {
+		DontDestroyOnLoad (gameObject);
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		//string date = System.DateTime.Now.Date.ToString ();
+
+		timeDisplay.text = System.DateTime.Now.ToShortTimeString();
+		//GetComponent<Text> ().text = time;
+		InvokeRepeating("TimeUpdate", 0, 1.0f);
+
+	}
+
+	// TimeUpdate is called every second
+	void TimeUpdate(){ 
+		if (System.DateTime.Now.Minute == 0) {
+			if (System.DateTime.Now.Hour == 0){
+				// it's midnight
+				DailyUpdate();
+			}
+		}
+
+		timeDisplay.text = System.DateTime.Now.ToString ();
+	}
+
+	void DailyUpdate(){
+		// grow plants, update world...
+		return;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/Scripts/ToBeach.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToBeach : MonoBehaviour {
+
+	public void ClickTest(){
+		print ("clicked");
+		Application.LoadLevel ("beach");
+	}
+}
